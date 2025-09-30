94dde1a4f9bcc6d0014654d8c28cabdc861168f8
TonyTony-p <a.pizzonia.its2@gmail.com.com>
2025-09-30 19:51:01 +0200

    bottoni aggiustati, e centrati tutti gli elementi della tabella, aggiunto fr per inserire i dati nella tabella

--- src/app/cane-list/cane-list.spec.ts
+++ src/app/cane-list/cane-list.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CaneList } from './cane-list';
 
 describe('CaneList', () => {
--- src/app/cane-list/cane-list.ts
+++ src/app/cane-list/cane-list.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-cane-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cane-list.html',
   styleUrl: './cane-list.css'
 })
-export class CaneList {
+export class CaneList implements OnInit {
+  listaCani = [
+    {
+      nome: 'Cane1',
+      peso: 11,
+      razza: 'Labrador',
+      dataNascita: '15/03/2020',
+      pedigree: 'PED-1001'
+    },
+    {
+      nome: 'Cane2',
+      peso: 8,
+      razza: 'Beagle',
+      dataNascita: '10/07/2019',
+      pedigree: 'PED-1002'
+    }
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
