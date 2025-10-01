4eea41bc9afc50ea7cd72adede270322c0e32d57
Tonyy <a.pizzonia.its2@gmail.com>
2025-10-01 13:00:03 +0200

    rotte modificate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { CaneList } from './cane-list/cane-list';
 import { NewCane } from './new-cane/new-cane';
 
 export const routes: Routes = [
+
+    { path: '', redirectTo: 'cani', pathMatch: 'full' },
     {path : 'cani', component: CaneList},
     {path : 'newCane', component: NewCane}
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CaneList } from "./cane-list/cane-list";
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CaneList, FormsModule],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
